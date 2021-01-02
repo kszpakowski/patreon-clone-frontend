c772fdef7130ebc4ad578fea612f87eeee63a8cf
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-01-02 14:00:39 +0100

    add create post

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,13 @@
       <nuxt-link :to="sub.tier.owner.name" exact-active-class="is-active">
         span {{sub.tier.owner.name}} - {{sub.tier.name}} (Expires at: {{sub.expiresAt}})
       </nuxt-link> 
+
+  .createPostWrapper
+    h1 Create post
+    span Title
+    input(v-model="post.title" placeholder="title")
+    input(v-model="post.tierId" placeholder="tier")
+    button(type="button" @click="handleCreatePost") Post
 </template>
 
 <script>
@@ -14,6 +21,34 @@ import gql from 'graphql-tag'
 
 export default {
   components: {},
+  data() {
+    return {
+      post: {
+        title: '',
+        tierId: '',
+      },
+    }
+  },
+  methods: {
+    async handleCreatePost() {
+      const post = {
+        title: this.post.title,
+        tierId: parseInt(this.post.tierId),
+      }
+      const res = await this.$apollo.mutate({
+        mutation: gql`
+          mutation CreatePostMutation($title: String!, $tierId: Int!) {
+            createPost(createPostInput: { title: $title, tierId: $tierId }) {
+              id
+              title
+            }
+          }
+        `,
+        variables: post,
+      })
+      console.log(res)
+    },
+  },
   apollo: {
     me: gql`
       {
