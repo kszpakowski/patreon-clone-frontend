93c65e17c7853365b91866f6996e76b7b611bdcc
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-01-10 13:11:45 +0100

    add posts to the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,52 @@
 <template lang="pug">
 .wrapper
-  .columns(v-if="me") 
-    .column.is-one-quarter
-      .section
-        p.title My subscriptions
-        ul
-          li(v-for="sub in me.subscriptions")
-            nuxt-link(:to="sub.tier.owner.name" exact-active-class="is-active")
-              p.title.is-4 {{sub.tier.owner.name}} - {{sub.tier.name}} 
-              p.subtitle.is-6 Expires at: {{sub.expiresAt}}
+  .container.has-text-centered(v-if="me")
+    .section
+      .box
+        p TODO: Content for logged in user
   .container.has-text-centered(v-else)
     .section
       .box
         p TODO: Content for not logged in user
+  .section
+    .post-list.container.column.is-two-fifths
+      .posts(v-for="post in posts")
+        .section
+          Post(:post="post" @liked="post.liked=true; post.likesCount +=1" @unliked="post.liked=false; post.likesCount -=1" @commented="(e) => {post.comments.push(e); post.commentsCount+=1;}")
 </template>
 
 <script>
-import PostComposer from '@/components/PostComposer'
 import gql from 'graphql-tag'
 
 export default {
-  components: { PostComposer },
   apollo: {
+    posts: gql`
+      {
+        posts {
+          id
+          title
+          createdAt
+          attachments {
+            url
+          }
+          commentsCount
+          comments {
+            createdAt
+            message
+            author {
+              name
+              avatarUrl
+            }
+          }
+          likesCount
+          liked
+          canLike
+          author {
+            name
+          }
+        }
+      }
+    `,
     me: gql`
       {
         me {
